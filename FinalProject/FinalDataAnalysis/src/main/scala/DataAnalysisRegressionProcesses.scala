27a663c55826805c901389136c6e3066cbd3e552
HaroldWu <wu.hao8@husky.neu.edu>
2020-11-27 12:02:04 -0500

    123

--- FinalProject/FinalDataAnalysis/src/main/scala/DataAnalysisRegressionProcesses.scala
+++ FinalProject/FinalDataAnalysis/src/main/scala/DataAnalysisRegressionProcesses.scala
@@ -1,44 +1,107 @@
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.parquet.format.IntType
+import org.apache.spark
 import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel}
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.{FeatureHasher, StringIndexer}
+import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
+import org.apache.spark.mllib.regression.LinearRegressionWithSGD
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object DataAnalysisRegressionProcesses {
   val DAC = new DataAnalysisController
 
   def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+    Logger.getRootLogger().setLevel(Level.ERROR) //这里是用来抑制一大堆log信息的.
 
-    val dataSet = DAC.processForRegression(DAC.loadData("AB_NYC_2019.csv")).cache()
-    val categoricalFeatures = Seq("neighbourhood_group", "room_type" )
-    val numericFeatures = Seq("latitude",
-      "longitude",
-      "price",
-      "minimum_nights",
-      "number_of_reviews",
-      "calculated_host_listings_count",
-      "availability_365")
+
+    DAC.spark.sparkContext.setLogLevel("ERROR")
+
+    val hadoopConf = DAC.spark.sparkContext.hadoopConfiguration
+    hadoopConf.set("fs.hdfs.impl", classOf[org.apache.hadoop.hdfs.DistributedFileSystem].getName)
+    hadoopConf.set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName)
+    var rdd= DAC.spark.sparkContext.parallelize(List("------------先大致看下数据-----------------"))
+    rdd.collect().foreach(println)
+
+    val df = DAC.loadData(DAC.PATH+".csv")
+    val numIterations = 100
+    val stepSize = 0.1
+    val lr = new LinearRegression()
+      .setMaxIter(numIterations)
+      .setRegParam(0.1)
+      .setElasticNetParam(0.1)
+
+    var model  = lr.fit(df)
+
+    println(s"Coefficients: ${model.coefficients} Intercept: ${model.intercept}")
+
+    val trainingSummary = model.summary
+    println(s"numIterations: ${trainingSummary.totalIterations}")
+    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+    trainingSummary.residuals.show()
+    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+    println(s"r2: ${trainingSummary.r2}")
+    trainingSummary.predictions.show()
+
+    DAC.spark.stop()
+
+    //---------------------------------------------------------
+    //Start
+  // var dataSet = .cache()
+//    val categoricalFeatures = Seq("neighbourhood_group", "room_type" )
+////    val numericFeatures = Seq("latitude",
+////      "longitude",
+////      "price",
+////      "minimum_nights",
+////      "number_of_reviews",
+////      "calculated_host_listings_count",
+////      "availability_365")
+//
+//    for(cata<- categoricalFeatures){
+//      val stringModel = new StringIndexer()
+//        .setInputCol(cata)
+//        .setOutputCol(cata.concat("_index"))
+//        .fit(dataSet)
+//
+//      val dataSet_enriched = stringModel.transform(dataSet)
+//      dataSet = dataSet_enriched
+//
+//    }
+//    dataSet.show()
+//
+//
+//    val lr_model = logisticRegModelGenerator(dataSet,DAC.spark)
+     DAC.spark.stop()
 
   }
 
   def logisticRegressionProcess(logisticRegressionModel: LogisticRegressionModel) :Unit = {
 
   }
-  def logisticRegModelGenerator(df :DataFrame,sc: SparkSession) : LogisticRegressionModel ={
+  def logisticRegModelGenerator(df:DataFrame ,sc: SparkSession) : LogisticRegressionModel ={
     import sc.implicits._
-
-
-    val featureColumns = Array("latitude","longitude","reviews_per_month","price")
-
-    val indexLabelColunm = ""
 
     val splitSeed = 666
     val Array(trainData,testData) = df.randomSplit(Array(0.8,0.2),splitSeed)
-    val lr = new LogisticRegression().setMaxIter(370).setRegParam(0.01).setElasticNetParam(0.92)
-    val model = lr.fit(trainData)
-    val predictions = model.transform(testData)
+    val lr = new LogisticRegression()
+      .setMaxIter(370)
+      .setRegParam(0.01)
+      .setElasticNetParam(0.92)
+      .setFeaturesCol("feature")
+      .setPredictionCol("pre_result")
+      .setLabelCol("price")
+
+    val model_lr = lr.fit(trainData)
+    println(s"每个特征对应系数: ${model_lr.coefficients} 截距: ${model_lr.intercept}")
+    val predictions = model_lr.transform(testData)
     predictions.show(30)
     val evaluator = new BinaryClassificationEvaluator()
-      .setLabelCol("label")
+      .setLabelCol("price")
       .setRawPredictionCol("rawPrediction")
       .setMetricName("areaUnderROC")
 
@@ -62,7 +125,10 @@ object DataAnalysisRegressionProcesses {
     println(s"False Positives: $falseP")
     println(s"Ratio of wrong results: $ratioWrong")
     println(s"Ration of correct results: $ratioCorrect")
-
-    model
+    println(s"Accuracy is:  $accuracy")
+    model_lr
   }
+//  def linearRegressionProcess(df:DataFrame, sc:SparkSession): LinearRegressionModel={
+//
+//  }
 }
--- FinalProject/finalDataAnalysis/src/main/scala/DataAnalysisController.scala
+++ FinalProject/finalDataAnalysis/src/main/scala/DataAnalysisController.scala
@@ -1,11 +1,40 @@
+import org.apache.spark.ml.feature.{LabeledPoint, StringIndexer}
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.types.{DoubleType, IntegerType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
+
 class DataAnalysisController {
   val spark: SparkSession = SparkSession.builder().master("local").getOrCreate()
+  val PATH:String = "dataSet"
+  val OLD_PATH:String = "AB_NYC_2019.csv"
+  def loadData(file: String):DataFrame = {
+    import spark.implicits._
+      val dataSet = spark.sparkContext.textFile(file)
+      val header = dataSet.first()
+      val dataSet1 = dataSet.filter(row=> row!=header)
 
-  def loadData(file: String) = {
+      val parsedData = dataSet1.map{line=>
+        val parts = line.split(",")
+        LabeledPoint(parts(0).toDouble,
+          Vectors.dense(parts(1).toDouble
+            ,parts(2).toDouble
+            ,parts(3).toDouble
+            ,parts(4).toDouble
+            ,parts(5).toDouble
+            ,parts(6).toDouble
+            ,parts(7).toDouble
+            ,parts(8).toDouble
+            ,parts(9).toDouble
+          )
+          )
+      }.toDF("label","features")
+    parsedData.show()
+    parsedData
+  }
+  def storeCSVAfterClean(file:String) :Unit ={
     val dataSet = spark.read.
       option("header", "true")
       .option("mode", "DROPMALFORMED") // delete bad-formatted record
@@ -16,8 +45,29 @@ class DataAnalysisController {
       .na.drop() // delete rows which contains null/ NaN value
       .toDF()
       .cache()
-    dataSet.show(5,false)
-    dataSet
+    var df1 = dataSet
+      .drop("id")
+      .drop("name")
+      .drop("host_name")
+      .drop("host_id")
+      .drop("last_review")
+      .drop("neighbourhood")
+      .drop("last_review")
+    val catalog_features = Array("neighbourhood_group","room_type")
+    for(cf<-catalog_features) {
+      val indexer = new StringIndexer()
+
+        .setInputCol(cf)
+
+        .setOutputCol(cf.concat("_index"))
+      val train_index_model = indexer.fit(df1)
+      val df2 = train_index_model.transform(df1)
+      df1 = df2
+    }
+
+    val df2 = df1.drop("neighbourhood_group").drop("room_type")
+    df2.show()
+    df2.coalesce(1).write.option("header",true).csv(PATH)
   }
 
   def process(df: DataFrame): DataFrame = {
@@ -26,7 +76,6 @@ class DataAnalysisController {
       .withColumn("longitude", col("longitude").cast(DoubleType))
       .withColumn("latitude", col("latitude").cast(DoubleType))
       .drop("name")
-      .drop("id")
       .drop("host_name")
       .drop("last_review")
     df1.createOrReplaceTempView("df1")
@@ -47,11 +96,16 @@ class DataAnalysisController {
       .drop("host_name")
       .drop("last_review")
       .drop("host_id")
+      .drop("calculated_host_listings_count")
+      .drop("availability_365")
       .drop("neighbourhood") //optional
       .drop("reviews_per_month") //optional
+      .withColumn("price",col("price").cast(IntegerType))
     df1.createOrReplaceTempView("df1")
 
-    spark.sql("SELECT * FROM df1 WHERE latitude <= 41")
+    val res = spark.sql("SELECT * FROM df1 WHERE latitude <= 41")
+    res.show(5,false)
+    res
   }
 
   def processForClassification(df: DataFrame): DataFrame = {
@@ -65,8 +119,4 @@ class DataAnalysisController {
     df1.createOrReplaceTempView("df1")
     spark.sql("SELECT * FROM df1 WHERE latitude <= 41")
   }
-
-
-
-
 }
